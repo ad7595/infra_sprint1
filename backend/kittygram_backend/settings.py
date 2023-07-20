9225325062d66bad384de2ca78228f6f5627cddc
a d <116667317+ad7595@users.noreply.github.com>
2023-07-20 18:09:14 +0700

    commit 4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SECRET_KEY = os.getenv('SECRET_KEY')
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(', ')
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
